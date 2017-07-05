f057537c88ed27a0f71303cb1b411d6445532775
Piotrek Bulik <bulik.piotr@gmail.com>
2017-07-05 22:13:05 +0200

    connected with database, created first view

--- dana/app_dana/views.py
+++ dana/app_dana/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+
+
+class MainView(View):
+
+    def get(self, request):
+        ctx = {}
+        return render(request, 'app_dana/main.html', ctx)
