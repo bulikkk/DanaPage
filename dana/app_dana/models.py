b75fd5f8e15814d55edb8b090e579ba0bb79dda9
Piotrek Bulik <bulik.piotr@gmail.com>
2017-07-10 19:14:35 +0200

    added icon "new" to projects and set to be visible 1 month

--- dana/app_dana/models.py
+++ dana/app_dana/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
 
@@ -14,6 +15,7 @@ class Project(models.Model):
     description = models.TextField()
     type = models.IntegerField(choices=TYPE_CHOICES.items())
     image = models.ImageField(upload_to="images")
+    add_date = models.DateField(default=date.today)
 
     def __str__(self):
         return '{}, {}'.format(self.title, self.description)
