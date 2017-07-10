e28ddea71963cefe74a134d86333eb9d7588b209
Piotrek Bulik <bulik.piotr@gmail.com>
2017-07-10 15:18:45 +0200

    added MySQL database, other folder for images to projects

--- dana/dana/settings.py
+++ dana/dana/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'dana.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'HOST': '127.0.0.1',
+        'NAME': 'dana_page',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'PASSWORD': 'coderslab',
+        'OPTIONS': {
+            'autocommit': True,
+        }
     }
 }
 
